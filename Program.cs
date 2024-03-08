eb9f739178c730acef6a5a2262f21a687905bcd4
callaito <radelinrsm029@gmail.com>
2024-03-08 08:37:00 -0400

    Se agrega el Proyecto

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Double numero;
+
+Console.Write("numero: ");
+numero = Convert.ToDouble(Console.ReadLine());
+
+if (numero %2 == 0)
+{
+    Console.WriteLine("El numero es par");
+}
+else
+    Console.WriteLine("El numero es impar");
